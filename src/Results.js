6d269f55475f861662f8955fab53001f42848bc1
Ejim Michael <ejimmichael1@gmail.com>
2021-08-06 23:57:23 +0100

    use axios to make fetch requests

--- src/Results.js
+++ src/Results.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 import './Results.css'
 import VideoCard from './VideoCard'
+import axios from './axios';
+import requests from './requests';
 
 function Results() {
-    return <div className="results">
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard /> 
-        <VideoCard />       
+    const [movies, setMovies] = useState([]);
+
+        useEffect(() => {
+            async function fetchData() {
+            const request = await axios.get(requests.fetchActionMovies);
+            console.log(request);
+            }
+            fetchData();
+        }, []);
+    
+    return (
+        <div className="results">
+            {movies.map(movie => {
+                <VideoCard />
+            })}
+      
     </div>
+    )
 }
 
 export default Results
